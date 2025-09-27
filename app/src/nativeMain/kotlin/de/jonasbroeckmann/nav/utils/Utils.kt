b87906ebb7d2b92629081afd222c041ef97e5c5a
JojoIV <jonas.broeckmann@gmx.de>
2025-09-27 02:11:06 +0200

    Apply detekt to all source sets but common

--- app/src/nativeMain/kotlin/de/jonasbroeckmann/nav/utils/Utils.kt
+++ app/src/nativeMain/kotlin/de/jonasbroeckmann/nav/utils/Utils.kt
@@ -4,8 +4,6 @@ import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKString
 
 @OptIn(ExperimentalForeignApi::class)
-actual fun getenv(key: String): String? {
-    return platform.posix.getenv(key)?.toKString()
-}
+actual fun getenv(key: String): String? = platform.posix.getenv(key)?.toKString()
 
 actual fun exitProcess(status: Int): Nothing = kotlin.system.exitProcess(status)
