411a125b8825ee0edd870e32c707f011b73b382a
JojoIV <jonas.broeckmann@gmx.de>
2025-09-28 00:56:32 +0200

    Add FileAttributes class for Windows file attribute handling

---
+++ app/src/mingwMain/kotlin/de/jonasbroeckmann/nav/utils/FileAttributes.kt
@@ -0,0 +1,34 @@
+package de.jonasbroeckmann.nav.utils
+
+import kotlinx.io.files.Path
+import platform.windows.*
+
+value class FileAttributes(private val raw: UInt) {
+    val isReadOnly get() = raw mask FILE_ATTRIBUTE_READONLY
+    val isHidden get() = raw mask FILE_ATTRIBUTE_HIDDEN
+    val isSystem get() = raw mask FILE_ATTRIBUTE_SYSTEM
+    val isDirectory get() = raw mask FILE_ATTRIBUTE_DIRECTORY
+    val isArchive get() = raw mask FILE_ATTRIBUTE_ARCHIVE
+    val isDevice get() = raw mask FILE_ATTRIBUTE_DEVICE
+    val isNormal get() = raw mask FILE_ATTRIBUTE_NORMAL
+    val isTemporary get() = raw mask FILE_ATTRIBUTE_TEMPORARY
+    val isSparseFile get() = raw mask FILE_ATTRIBUTE_SPARSE_FILE
+    val isReparsePoint get() = raw mask FILE_ATTRIBUTE_REPARSE_POINT
+    val isCompressed get() = raw mask FILE_ATTRIBUTE_COMPRESSED
+    val isOffline get() = raw mask FILE_ATTRIBUTE_OFFLINE
+    val isNotContentIndexed get() = raw mask FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
+    val isEncrypted get() = raw mask FILE_ATTRIBUTE_ENCRYPTED
+    val isIntegrityStream get() = raw mask 0x00008000
+    val isVirtual get() = raw mask FILE_ATTRIBUTE_VIRTUAL
+    val isNoScrubData get() = raw mask 0x00020000
+    val hasExtendedAttributes get() = raw mask 0x00040000
+    val isPinned get() = raw mask 0x00080000
+    val isUnpinned get() = raw mask 0x00100000
+    val isRecallOnOpen get() = raw mask 0x00040000
+    val isRecallOnDataAccess get() = raw mask 0x00400000
+}
+
+fun Path.fileAttributes() = when (val attributes = GetFileAttributesW(toString())) {
+    INVALID_FILE_ATTRIBUTES -> null
+    else -> FileAttributes(attributes)
+}
