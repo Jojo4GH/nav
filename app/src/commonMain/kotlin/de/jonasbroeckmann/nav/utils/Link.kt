252e5796f695383bf6cee377d9402a93aca4779b
Erik Oestlyngen <erik.oestlyngen@gmx.de>
2025-09-28 01:49:21 +0200

    fix formatting

--- app/src/commonMain/kotlin/de/jonasbroeckmann/nav/utils/Link.kt
+++ app/src/commonMain/kotlin/de/jonasbroeckmann/nav/utils/Link.kt
@@ -2,5 +2,4 @@ package de.jonasbroeckmann.nav.utils
 
 import kotlinx.io.files.Path
 
-
 expect fun readLink(path: Path): String?
--- app/src/jvmMain/kotlin/de/jonasbroeckmann/nav/utils/Link.jvm.kt
+++ app/src/jvmMain/kotlin/de/jonasbroeckmann/nav/utils/Link.jvm.kt
@@ -3,6 +3,6 @@ package de.jonasbroeckmann.nav.utils
 import kotlinx.io.files.Path
 
 actual fun readLink(path: Path): String? {
-    //TODO: Just a dummy value. Before doing this need we need to be able to even recognize symlinks under jvm
+    // TODO: Just a dummy value. Before doing this need we need to be able to even recognize symlinks under jvm
     return null
 }
