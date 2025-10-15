d327037f3e52d796c9effb2f81f374dc6f1e56ab
JojoIV <jonas.broeckmann@gmx.de>
2025-10-16 01:04:00 +0200

    Temporary changes to address compiler issue

--- framework/src/commonMain/kotlin/de/jonasbroeckmann/nav/framework/action/KeyActions.kt
+++ framework/src/commonMain/kotlin/de/jonasbroeckmann/nav/framework/action/KeyActions.kt
@@ -59,11 +59,11 @@ abstract class KeyActions<Context, Controller, Category>(vararg validInputModes:
     )
 
     companion object {
-        protected fun <Context, Controller> KeyActions<Context, Controller, Unit>.registerKeyAction(
+        /* protected */ fun <Context, Controller> KeyActions<Context, Controller, Unit>.registerKeyAction(
             action: KeyAction<Context, Controller>
         ) = Unit.registerKeyAction(action)
 
-        protected fun <Context, Controller> KeyActions<Context, Controller, Unit>.registerKeyAction(
+        /* protected */ fun <Context, Controller> KeyActions<Context, Controller, Unit>.registerKeyAction(
             vararg keys: KeyboardEvent,
             displayKey: Context.() -> KeyboardEvent? = { keys.firstOrNull() },
             description: Context.() -> String = { "" },
@@ -80,5 +80,41 @@ abstract class KeyActions<Context, Controller, Category>(vararg validInputModes:
             condition = condition,
             action = action
         )
+
+
+
+        /*
+         * The above two functions should be protected.
+         * But because of the following issue in the compiler, they are public for now:
+         * Task: compileKotlinJvm FAILED
+         * java.lang.AssertionError:
+         * SyntheticAccessorLowering should not attempt to modify other files!
+         * While lowering this file:
+         *     FILE
+         *     fqName:de.jonasbroeckmann.nav.app.actions
+         *     fileName:/.../nav/app/src/commonMain/kotlin/de/jonasbroeckmann/nav/app/actions/NormalModeActions.kt
+         * Trying to add this accessor:
+         *     FUN SYNTHETIC_ACCESSOR
+         *     name:access$registerKeyAction
+         *     visibility:public
+         *     modality:FINAL
+         *     <Context, Controller>
+         *     (
+         *         $this:de.jonasbroeckmann.nav.framework.action.KeyActions.Companion,
+         *         $receiver:de.jonasbroeckmann.nav.framework.action.KeyActions<
+         *             Context of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction,
+         *             Controller of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction,
+         *             kotlin.Unit
+         *         >,
+         *         action:de.jonasbroeckmann.nav.framework.action.KeyAction<
+         *             Context of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction,
+         *             Controller of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction
+         *         >
+         *     )
+         *     returnType:de.jonasbroeckmann.nav.framework.action.KeyAction<
+         *         Context of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction,
+         *         Controller of de.jonasbroeckmann.nav.framework.action.KeyActions.Companion.access$registerKeyAction
+         *     >
+         */
     }
 }
